34be0c85aaf01c261ba70afce525e14f8c03c45f
gabengcui <hongyaoyun@gmail.com>
2023-11-03 15:14:28 +0800

    feat: 生成ir

--- 编译/sysy-cmake-template/src/expr_ast.rs
+++ 编译/sysy-cmake-template/src/expr_ast.rs
@@ -2,27 +2,62 @@
 pub struct CompUnit {
     pub func_def: FuncDef,
 }
-     
-#[derive(Debug)]     
+impl CompUnit {
+    pub fn gen_ir(&self) -> String {
+        return self.func_def.gen_ir();
+    }
+}
+
+#[derive(Debug)]
 pub struct FuncDef {
     pub func_type: FuncType,
     pub ident: String,
     pub block: Block,
+}
+impl FuncDef {
+    pub fn gen_ir(&self) -> String {
+        return "fun @".to_owned()
+            + self.ident.as_str()
+            + "():"
+            + self.func_type.gen_ir().as_str()
+            + "{"
+            + "%entry:"
+            + "ret "
+            + self.block.gen_ir().as_str()
+            + "}";
+    }
 }
 
 #[derive(Debug)]
 pub struct Block {
     pub stmt: Stmt,
 }
+impl Block {
+    pub fn gen_ir(&self) -> String {
+        return self.stmt.gen_ir();
+    }
+}
 
 #[derive(Debug)]
 pub struct Stmt {
     pub num: Number,
 }
+impl Stmt {
+    pub fn gen_ir(&self) -> String {
+        return self.num.to_string();
+    }
+}
 
 #[derive(Debug)]
 pub enum FuncType {
- Int,
+    Int,
+}
+impl FuncType {
+    pub fn gen_ir(&self) -> String {
+        match self {
+            FuncType::Int => "i32".to_owned(),
+        }
+    }
 }
 
 use i32 as Number;
--- 编译/sysy-cmake-template/src/main.rs
+++ 编译/sysy-cmake-template/src/main.rs
@@ -2,7 +2,6 @@ use lalrpop_util::lalrpop_mod;
 use std::env::args;
 use std::fs::read_to_string;
 use std::io::Result;
-// use crate::expr_ast;
 mod expr_ast;
 
 
@@ -26,27 +25,6 @@ fn main() -> Result<()> {
   let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
 
   // 输出解析得到的 AST
-  println!("{:#?}", ast);
+  println!("{:#?}", ast.gen_ir());
   Ok(())
 }
-
-
-
-// use std::io;
-// use rand::Rng;
-
-// fn main() {
-//     println!("Guess the number!");
-//     let secret_number = rand::thread_rng().gen_range(1..=100);
-//     println!("The secret number is: {secret_number}");
-
-//     println!("Please input your guess.");
-
-//     let mut guess = String::new();
-
-//     io::stdin()
-//         .read_line(&mut guess)
-//         .expect("Failed to read line");
-
-//     println!("You guessed: {guess}");
-// }
