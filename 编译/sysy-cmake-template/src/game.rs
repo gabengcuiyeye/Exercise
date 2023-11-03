5384362919fcc739638708bb79fef29f5df7e4f9
gabengcui <hongyaoyun@gmail.com>
2023-11-03 17:10:32 +0800

    feat: rust入门

---
+++ 编译/sysy-cmake-template/src/game.rs
@@ -0,0 +1,24 @@
+
+  
+
+pub fn test(){
+    use std::io;
+    use rand::Rng;
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");  
+}
+
+
